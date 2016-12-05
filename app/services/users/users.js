897a750fe86b43eb883ad83eb7c75c555dab786a
schukanov <schukanov@it.ru>
2016-12-05 16:53:01 +0300

    First commit

---
+++ app/services/users/users.js
@@ -0,0 +1,13 @@
+(function() {
+  'use strict';
+
+  // Creating the module and factory we referenced in the beforeEach blocks in our test file
+  angular.module('api.users', [])
+  .factory('Users', function() {
+    var Users = {};
+
+    // Users.method = function() {};
+
+    return Users;
+  });
+})();
